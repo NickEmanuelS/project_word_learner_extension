50e9ebdcf1e1e88dae03f209796112f259d61681
NickEmanuelS <nicolasdipinho@gmail.com>
2024-05-30 10:45:33 -0300

    create repository

---
+++ extension/popup.js
@@ -0,0 +1,18 @@
+document.getElementById('sendUrl').addEventListener('click', () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    let activeTab = tabs[0];
+    if (activeTab.url) {
+      fetch('http://127.0.0.1:8000/api/crawl_and_save_words/', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3MDkzODc3LCJpYXQiOjE3MTcwNzU4NzcsImp0aSI6IjVmMjRiNzA3ZDM4MTQ5MWI4NTA4NDg0YWFhZTYzOGMwIiwidXNlcl9pZCI6MSwiZW1haWwiOiJuaWNvQGdtYWlsLmNvbSIsIm5hbWUiOiJOaWNvbGFzIEVtYW51ZWwiLCJ1c2VybmFtZSI6Im5pY29fZW1hbnVlbCJ9.WfN9YimGBqPr6JdAzc52s8q0ag8Bv8LRnrtKjo76GB4'
+        },
+        body: JSON.stringify({ url: activeTab.url })
+      })
+      .then(response => response.json())
+      .then(data => console.log(data))
+      .catch(error => console.error('Erro:', error));
+    }
+  });
+});
