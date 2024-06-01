9719042551c3c952ad21063ba77ab7559b7eeb5a
NickEmanuelS <nicolasdipinho@gmail.com>
2024-06-01 11:41:24 -0300

    update token

--- extension/background.js
+++ extension/background.js
@@ -4,7 +4,7 @@ chrome.action.onClicked.addListener((tab) => {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3MDkzODc3LCJpYXQiOjE3MTcwNzU4NzcsImp0aSI6IjVmMjRiNzA3ZDM4MTQ5MWI4NTA4NDg0YWFhZTYzOGMwIiwidXNlcl9pZCI6MSwiZW1haWwiOiJuaWNvQGdtYWlsLmNvbSIsIm5hbWUiOiJOaWNvbGFzIEVtYW51ZWwiLCJ1c2VybmFtZSI6Im5pY29fZW1hbnVlbCJ9.WfN9YimGBqPr6JdAzc52s8q0ag8Bv8LRnrtKjo76GB4'
+          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3MTEyNjUxLCJpYXQiOjE3MTcwOTQ2NTEsImp0aSI6IjA2MDZiMjYwZTQ2YjRmZjA4ZmRiZmIwZmZmYzA2NjQ4IiwidXNlcl9pZCI6MSwiZW1haWwiOiJuaWNvQGdtYWlsLmNvbSIsIm5hbWUiOiJOaWNvbGFzIEVtYW51ZWwiLCJ1c2VybmFtZSI6Im5pY29fZW1hbnVlbCJ9.o-M6VKzI2l7KiwdSRwpVry3tO4MMS38ln1SwIyXVMfo'
         },
         body: JSON.stringify({ url: tab.url })
       })
--- extension/popup.js
+++ extension/popup.js
@@ -2,17 +2,47 @@ document.getElementById('sendUrl').addEventListener('click', () => {
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     let activeTab = tabs[0];
     if (activeTab.url) {
-      fetch('http://127.0.0.1:8000/api/crawl_and_save_words/', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbl90eXBlIjoiYWNjZXNzIiwiZXhwIjoxNzE3MDkzODc3LCJpYXQiOjE3MTcwNzU4NzcsImp0aSI6IjVmMjRiNzA3ZDM4MTQ5MWI4NTA4NDg0YWFhZTYzOGMwIiwidXNlcl9pZCI6MSwiZW1haWwiOiJuaWNvQGdtYWlsLmNvbSIsIm5hbWUiOiJOaWNvbGFzIEVtYW51ZWwiLCJ1c2VybmFtZSI6Im5pY29fZW1hbnVlbCJ9.WfN9YimGBqPr6JdAzc52s8q0ag8Bv8LRnrtKjo76GB4'
-        },
-        body: JSON.stringify({ url: activeTab.url })
-      })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('Erro:', error));
+      const username = document.getElementById('username').value;
+      const password = document.getElementById('password').value;
+
+      if (username && password) {
+        const url = 'http://127.0.0.1:8000/api/token/';
+        const data = {
+          username: username,
+          password: password
+        };
+
+        fetch(url, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json'
+          },
+          body: JSON.stringify(data)
+        })
+        .then(response => response.json())
+        .then(data => {
+          if (data.access) {
+            const token = data.access;
+
+            fetch('http://127.0.0.1:8000/api/crawl_and_save_words/', {
+              method: 'POST',
+              headers: {
+                'Content-Type': 'application/json',
+                'Authorization': 'Bearer ' + token
+              },
+              body: JSON.stringify({ url: activeTab.url })
+            })
+            .then(response => response.json())
+            .then(data => console.log(data))
+            .catch(error => console.error('Erro:', error));
+          } else {
+            console.error('Erro ao obter o token:', data);
+          }
+        })
+        .catch(error => console.error('Erro ao fazer a solicitação de token:', error));
+      } else {
+        console.error('Por favor, insira o nome de usuário e senha.');
+      }
     }
   });
 });
